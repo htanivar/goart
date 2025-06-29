a4f7fc68c5cfeaa33f23fde465c509f5825f4e17
Ravi J <raviregi@gmail.com>
2025-06-29 16:28:11 +0530

    updated tests

--- os/check_test.go
+++ os/check_test.go
@@ -32,3 +32,31 @@ func TestIsPathExists(t *testing.T) {
 		t.Errorf("Expected file %s to NOT be recognized as directory", file.Name())
 	}
 }
+
+func TestIsFileExists(t *testing.T) {
+	// Create a temp directory
+	dir := t.TempDir()
+
+	// Test: directory should not be a file
+	if IsFileExists(dir) {
+		t.Errorf("Expected directory %s to NOT be recognized as a file", dir)
+	}
+
+	// Test: non-existent path
+	nonExistent := dir + "/notexist"
+	if IsFileExists(nonExistent) {
+		t.Errorf("Expected non-existent path %s to NOT be recognized as a file", nonExistent)
+	}
+
+	// Create a temp file
+	file, err := os.CreateTemp(dir, "file")
+	if err != nil {
+		t.Fatalf("Unable to create temp file: %v", err)
+	}
+	file.Close()
+
+	// Test: file should be recognized as file
+	if !IsFileExists(file.Name()) {
+		t.Errorf("Expected file %s to exist and be recognized as a file", file.Name())
+	}
+}
